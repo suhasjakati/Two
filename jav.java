dfd657ca98d880b26560322bada355e00c031431
suhasjakati219@gmail.com <suhasjakati219@gmail.com>
2025-04-03 19:26:10 +0530

    save

---
+++ jav.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class javaex {
+    javaex() {
+    }
+ 
+    public static void main(String[] var0) {
+       System.out.println("Hello, World!");
+    }
+ }
+ 
